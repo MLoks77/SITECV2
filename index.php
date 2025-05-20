6f8285d659f2ec4654fbe2b547bffa9e57e66f58
MLoks77 <maximederenes@gmail.com>
2025-05-20 10:32:49 +0200

    20/5 10H30

--- index.php
+++ index.php
@@ -2,23 +2,40 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Mon Site</title>
+    <title>Maxime Derènes</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Ajoutez vos liens CSS ici -->
-    <link rel="stylesheet" href="styles.css">
+
+    <meta name="theme-color" content="#12142b" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Maxime Derènes" />
+    <meta property="og:description" content="Bienvenue sur le site portfolio de Maxime Derènes, un étudiant en BUT MMI à l'IUT de Meaux en France." />
+    <meta property="og:url" content="https://maxime-derenes.fr/" />
+    <meta property="og:site_name" content="Maxime Derènes Portfolio" />
+    <meta name="keywords" content="Maxime Derènes, Maxime Derenes, maxime derenes, maxime derènes, programmation, développeur, graphisme, portfolio, CV, BUT MMI">
+    <meta property="og:locale" content="fr_FR" />
+
+    <link rel="icon" type="image/png" href="images/favicon.ico">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="js/index.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+
 </head>
 <body>
     <?php
-    // Le code PHP peut aller ici
+    include 'include/navbar.php';
     ?>
     <header>
-        <h1>Bienvenue sur Mon Site</h1>
+        <h1>test</h1>
     </header>
+
     <main>
-        <p>Ceci est une page modèle.</p>
+        <p>test</p>
     </main>
+
     <footer>
-        <p>&copy; <?php echo date("Y"); ?> Mon Site</p>
+    <?php
+    include 'include/footer.php';
+    ?>
     </footer>
 </body>
 </html>
